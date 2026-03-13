5839f68c98174d06e7a179afc606ba788d166b07
Artem Klimenko <klimenkocpp@icloud.com>
2026-03-14 02:59:09 +0300

    feat: update NavGraph to include NotesListScreen

--- app/src/main/java/com/ultrawork/notes/navigation/NavGraph.kt
+++ app/src/main/java/com/ultrawork/notes/navigation/NavGraph.kt
@@ -1,8 +1,13 @@
 package com.ultrawork.notes.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.ultrawork.notes.ui.screens.NotesListScreen
 
 object Routes {
     const val LOGIN = "login"
@@ -18,8 +23,23 @@ fun NavGraph() {
 
     NavHost(
         navController = navController,
-        startDestination = Routes.LOGIN
+        startDestination = Routes.NOTES_LIST
     ) {
-        // TODO: Add screen destinations
+        composable(Routes.NOTES_LIST) {
+            val viewModel = hiltViewModel<com.ultrawork.notes.viewmodel.NotesViewModel>()
+            NotesListScreen(viewModel = viewModel)
+        }
+        
+        // TODO: Add other screen destinations
+        // composable(Routes.LOGIN) { ... }
+        // composable(Routes.REGISTER) { ... }
+        // composable(
+        //     route = Routes.NOTE_DETAIL,
+        //     arguments = listOf(navArgument("noteId") { type = NavType.LongType })
+        // ) { ... }
+        // composable(
+        //     route = Routes.NOTE_EDIT,
+        //     arguments = listOf(navArgument("noteId") { type = NavType.LongType })
+        // ) { ... }
     }
 }
